a891b56681cbda88820f3141f5ca831be50c561c
hilman khitam <hilmankhitam100@gmail.com>
2023-10-28 11:46:23 +0800

    use client on info components

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,10 +1,9 @@
-import getProduct from '@/actions/get-product';
-import getProducts from '@/actions/get-products';
+import ProductList from '@/components/product-list';
 import Gallery from '@/components/gallery';
 import Info from '@/components/info';
-import ProductList from '@/components/product-list';
+import getProduct from '@/actions/get-product';
+import getProducts from '@/actions/get-products';
 import Container from '@/components/ui/container';
-import React from 'react'
 
 export const revalidate = 0;
 
@@ -18,7 +17,11 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
     const product = await getProduct(params.productId);
     const suggestedProducts = await getProducts({
         categoryId: product?.category?.id
-    })
+    });
+
+    if (!product) {
+        return null;
+    }
     return (
         <div className='bg-white'>
             <Container>
@@ -26,7 +29,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
                     <div className='lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8'>
                         <Gallery images={product.images} />
                         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                            <Info data={product}/>
+                            <Info data={product} />
                         </div>
                     </div>
                     <hr className='my-10' />
